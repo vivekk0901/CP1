b9eb4ba0c40197fc03cd4ccf28387191a942da5f
vivekk0901 <tojizenin711@gmail.com>
2026-08-13 23:50:23 +0530

    Add files via upload

---
+++ p1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter Your Two Numbers =");
+	scanf("%d %d",&a,&b);
+	printf("Sum = %d\n",a+b);
+	printf("Subtraction = %d\n",a-b);
+	printf("Multiplication = %d\n",a*b);
+	printf("Division = %d\n",a/b);
+}
